024cb6235f5ae0950a1f274eebe22085338f398c
罄天 <qinliang.ql@alibaba-inc.com>
2017-01-08 11:49:32 +0800

    添加注释

--- html-template-loader/index.js
+++ html-template-loader/index.js
@@ -12,6 +12,7 @@ module.exports = function(source, map) {
         meetGenerateScript = false;
 
     var onlyOneTpl = true;
+    //表示是否仅仅只有tpl为'__only__'
 
     source.split(/\r?\n/).forEach(function(line){
         line = line.trim();
@@ -24,6 +25,11 @@ module.exports = function(source, map) {
             variable = regArray[1];
             tpl += "'',\n" + variable + ":";
             onlyOneTpl = false;
+            /*
+             此时得到如下的样式：
+                "__only__: '',
+                name:"
+            */
         //prepare script start
         } else if(/<script\s+generate/.test(line)){
             prepareScript += line;
@@ -60,6 +66,19 @@ module.exports = function(source, map) {
         } else if(meetGenerateScript){
             tpl += line + "+\n";
         } else {
+            /*
+             tpl=  "__only__: '',
+                name:"
+            此时如果line1='<div>',那么结果就是：
+                "__only__: '',
+                name:'<div>'+
+                "
+            代码line.replace(/'/g, "\\'")就是把单引号转化为双引号：
+              '<div>'.replace(/'/g, "\\'")
+              得到"<div>"
+            注意：这里牵涉到引号逻辑，因为一开始最外面是双引号，所以如果你希望保留单引号那么久需要单独添加，不需要保留
+                引号的地方直接采用双引号即可！
+            */
             tpl += "'" + line.replace(/'/g, "\\'") + "'+\n";
         }
     });
@@ -71,19 +90,13 @@ module.exports = function(source, map) {
     } else {
         tpl = tpl.replace("__only__: '',", "");
         tpl = "var tpl = {" + tpl + "}";
+        //转化成为可以直接eval的字符串
     }
-    //var html = eval(tpl.email);
-    //console.log(JSON.stringify(html));
-
     map = {};
     source = "";
-    //this.callback(null, source, {hello: "world"}); 
     var abc = 5;
     var exports = prepareScript + "\nmodule.exports = tpl";
-    //console.log("-------------------------exports------------------------");
-    //console.log(exports);
-    //console.log("------------------------tpl-----------------------");
-    //console.log(tpl);
+    //如果没有处理script添加的那种模板，那么我们这里的prepareScript就是""空字符串
     return prepareScript + "\n" + tpl + "\nmodule.exports = tpl";
 
 }
